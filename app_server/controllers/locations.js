2f7ce0b19b5900e32bf3254ae1828fd03c48655c
Shantanu Patel <shantu.patel@gmail.com>
2025-05-26 13:00:14 +0530

    Controller for Locations added.

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,17 @@
+/* GET 'home' page */
+module.exports.homelist = function(req, res) {
+  res.render('index', { title: 'Home' });
+};
+
+/* GET 'location info' page */
+module.exports.locationInfo = function(req, res) {
+  res.render('index', { title: 'Location info' });
+};
+
+/* GET 'Add review' page */
+module.exports.addReview = function(req, res) {
+  res.render('index', { title: 'Add review' });
+};
+
+// alternative way to exporting all the modules via a single export as compared to individual exports
+// module.exports = { homelist, locationInfo, addReview };
