bf7e2cdc92f183cbbe46417c59768a04b230e6ba
Shantanu Patel <shantu.patel@gmail.com>
2025-05-30 12:09:40 +0530

    locations view updated by making use of location-info and location-list jade templates.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,12 +5,12 @@ module.exports.homelist = function(req, res) {
 
 /* GET 'location info' page */
 module.exports.locationInfo = function(req, res) {
-  res.render('index', { title: 'Location info' });
+  res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res) {
-  res.render('index', { title: 'Add review' });
+  res.render('location-review-form', { title: 'Add review' });
 };
 
 // alternative way to exporting all the modules via a single export as compared to individual exports
