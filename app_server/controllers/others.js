2ab3f1027d5a23ad2fb7061d404a7dbb10eb7eac
Shantanu Patel <shantu.patel@gmail.com>
2025-05-30 12:11:13 +0530

    Others controller file updated by making use of generic-text view template.

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,6 @@
 /* GET 'about' page */
 const others = function(req, res) {
-  res.render('index', { title: 'About' });
+  res.render('generic-text', { title: 'About' });
 }
 
 module.exports = { others }
